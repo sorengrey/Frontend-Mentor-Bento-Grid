2954421d53f2cfe87682ec07d8bb37e17b718b92
Heather Smith <halexsmith86@gmail.com>
2025-03-18 14:56:00 -0400

    Color styles
    Created custom colors according to Figma - changed hex code to semantic colors in html, changes to text styles

--- frontend-mentor-bento-grid/src/app/page.js
+++ frontend-mentor-bento-grid/src/app/page.js
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="min-h-screen py-[32px] px-[16px] bg-[#F6F5F6]">
       <div className="grid grid-cols-1 max-w-[375px] items-center justify-items-center gap-[32px] margin-auto">
-        <div className="flex flex-col headline p-[32px] bg-[#7551DC] inline-block text-center justify-items-center rounded-[10px] w-full">
+        <div className="flex flex-col headline p-[32px] bg-purple-500 inline-block text-center justify-items-center rounded-[10px] w-full">
           Social Media
-          <div className="flex text-[#FFCC6A] inline"> 10x </div>
+          <div className="flex text-yellow-500 inline"> 10x </div>
           <i>Faster </i>with AI
           <div className="mt-[24px] mb-[8px]">
             <Image
@@ -30,20 +30,20 @@ export default function Home() {
                 width="42"
                 height="42"
               />
-              @YourCo <div className="text-gray">12K Followers</div>
+              @YourCo <div className="text-preset-4">12K Followers</div>
             </div>
             <div className="flex flex-col padding-[12px] text-preset-1 shadow-sm rounded-[34px]">
               <Image src="/twitter.svg" alt="x logo" width="42" height="42" />
-              @YourCo <div className="text-gray">8k Followers</div>
+              @YourCo <div className="text-preset-4">8k Followers</div>
             </div>
           </div>
           Manage multiple accounts and platforms.
         </div>
-        <div className="bg-[#FFCC6A] items-center rounded-[10px] pt-[16px] px-[16px] w-full text-preset-3">
+        <div className="bg-yellow-500 items-center rounded-[10px] pt-[16px] px-[16px] w-full text-preset-3">
           Maintain a consistent posting schedule.
           <Image src="/calendar.svg" alt="calendar" width="208" height="157" />
         </div>
-        <div className="bg-[#DBD1FC] rounded-[10px] p-[16px] text-black">
+        <div className="bg-purple-100 rounded-[10px] p-[16px] text-black">
           <div className="text-preset-3 mb-[24px]">
             Schedule to social media.
           </div>
@@ -53,7 +53,7 @@ export default function Home() {
             your audience.
           </div>
         </div>
-        <div className="bg-[#7551DC] items-center rounded-[10px] p-[20px] text-white text-preset-3 w-full">
+        <div className="bg-purple-500 items-center rounded-[10px] p-[20px] text-white text-preset-3 w-full">
           <Image
             src="/followers.png"
             alt="growth chart"
@@ -66,9 +66,9 @@ export default function Home() {
           56% faster audience growth
           <Image src="/avatars.png" alt="avatars" width="178" height="66" />
         </div>
-        <div className="bg-[#F9EEE2] items-center rounded-[10px] p-[32px] text-black text-[32px] font-medium leading-[28px] tracking-[-0.06em] pb-[24px] w-full">
+        <div className="bg-yellow-100 items-center rounded-[10px] p-[32px] text-black text-[32px] font-medium leading-[28px] tracking-[-0.06em] pb-[24px] w-full">
           Create and schedule content
-          <div className="text-[#7551DC] inline mb-[24px]">
+          <div className="text-purple-500 inline mb-[24px]">
             <i> quicker.</i>
           </div>
           <div className="mt-[24px]">
@@ -80,7 +80,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="bg-[#FFCC6A] items-center rounded-[10px] p-[20px] text-black w-full">
+        <div className="bg-yellow-500 items-center rounded-[10px] p-[20px] text-black w-full">
           Write your content using AI.
           <Image src="/chat.png" alt="ai chat" width="219" height="222" />
         </div>
